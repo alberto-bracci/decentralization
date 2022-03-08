98936304e74d73334ac5ec111d07b6bc3509a5e2
Gabriele Dibona <ahw701@frontend8.apocrita>
2022-03-08 17:47:26 +0000

    updated requirements

--- bash_scripts/hsbm_equilibrate.sh
+++ bash_scripts/hsbm_equilibrate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -cwd  
-#$ -t 2-100
+#$ -t 1-100
 #$ -j y    
 #$ -pe smp 1
-#$ -l h_vmem=500G # check 2175659
+#$ -l h_vmem=100G # covid required ~ 42.5GB
 # #$ -l highmem
 #$ -l h_rt=240:0:0  
 # #$ -m bae
@@ -13,12 +13,9 @@ export OMP_NUM_THREADS=1
 conda activate gt
 cd ../scripts
 
-python hsbm.py --dataset_path "data/2022-01-01/covid/"  --do_analysis 0  -i ${SGE_TASK_ID} --stop_at_fit 0
+python hsbm.py --dataset_path "data/2022-01-01/decentralization/"  --do_analysis 0  -i ${SGE_TASK_ID} --stop_at_fit 0
 
 # Keywords: decentralization, covid, social_media, internet, wireless
 
 # If you want to submit a job to wait for the previous one to finish you can do so as follows:
-# qsub -hold_jid 2178832 job_two.sh ### where 2178832 is the id of the previous job to finish (covid fits 2-100)
-# 2178837 social_media fits 1-100
-# 2178838 internet fits 1-100
-# 2178839 wireless fits 1-100
+# qsub -hold_jid 500 job_two.sh ### where 500 is the id of the previous job to finish
